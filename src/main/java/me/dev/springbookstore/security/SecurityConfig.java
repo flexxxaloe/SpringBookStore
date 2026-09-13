95a1de9d3439f30d778f37b0e01f5f8b28e5ebbf
Konstantin <flexxxaloe2000@gmail.com>
2026-09-14 01:00:04 +0500

    public api documentation

--- src/main/java/me/dev/springbookstore/security/SecurityConfig.java
+++ src/main/java/me/dev/springbookstore/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/catalog/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/authors/**").permitAll()
                         .anyRequest()
